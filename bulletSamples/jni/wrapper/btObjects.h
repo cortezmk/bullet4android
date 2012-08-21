c3399a4445adb6f3651f1ed2a8d57110a346b6b7
Cortez <Cortez@Cortez-LAPTOP>
2012-08-21 19:27:42 +0200

    funkcje ułątwiające dostęp do obiektów za pośrednictwem id

--- bulletSamples/jni/wrapper/btObjects.h
+++ bulletSamples/jni/wrapper/btObjects.h
@@ -9,4 +9,34 @@ extern "C"
 		static int put(void* p);
 		static void* get(int p);
 	};
+
+	template<class T>
+	T* getObject(JNIEnv* env, jobject& obj)
+	{
+		jclass claz = env->GetObjectClass(obj);
+		jfieldID id = env->GetFieldID(claz, "id", "I");
+		return (T*)btObjects::get(env->GetIntField(obj, id));
+	}
+
+	void setNamedObject(JNIEnv* env, jobject& obj, char* name, int value)
+	{
+		jclass claz = env->GetObjectClass(obj);
+		jfieldID id = env->GetFieldID(claz, name, "I");
+		env->SetIntField(obj, id, value);
+	}
+
+	template<class T>
+	T* getNamedObject(JNIEnv* env, jobject& obj, char* name)
+	{
+		jclass claz = env->GetObjectClass(obj);
+		jfieldID id = env->GetFieldID(claz, name, "I");
+		return (T*)btObjects::get(env->GetIntField(obj, id));
+	}
+
+	int getIdNamedObject(JNIEnv* env, jobject& obj, char* name)
+	{
+		jclass claz = env->GetObjectClass(obj);
+		jfieldID id = env->GetFieldID(claz, name, "I");
+		return env->GetIntField(obj, id);
+	}
 }
