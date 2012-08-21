11818c54bd17fea2659b6adb27fd8c89091fa65a
Cortez <Cortez@Cortez-LAPTOP>
2012-08-22 00:20:57 +0200

    ...

--- bulletSamples/jni/wrapper/common.h
+++ bulletSamples/jni/wrapper/common.h
@@ -12,11 +12,21 @@ extern "C"
 }
 
 template<class T>
-T* getObject(JNIEnv* env, jobject& obj);
+T* getObject(JNIEnv* env, jobject& obj)
+{
+	jclass claz = env->GetObjectClass(obj);
+	jfieldID id = env->GetFieldID(claz, "id", "I");
+	return (T*)btObjects::get(env->GetIntField(obj, id));
+}
 
 void setNamedObject(JNIEnv* env, jobject& obj, char* name, int value);
 
 template<class T>
-T* getNamedObject(JNIEnv* env, jobject& obj, char* name);
+T* getNamedObject(JNIEnv* env, jobject& obj, char* name)
+{
+	jclass claz = env->GetObjectClass(obj);
+	jfieldID id = env->GetFieldID(claz, name, "I");
+	return (T*)btObjects::get(env->GetIntField(obj, id));
+}
 
 int getIdNamedObject(JNIEnv* env, jobject& obj, char* name);
